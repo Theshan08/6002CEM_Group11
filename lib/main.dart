1a9bec9139dab4d34f61f1a6f1c770a1f7415b79
Daniel <72720469+IAmDanieI@users.noreply.github.com>
2023-06-18 23:29:24 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'auth.dart';
 import 'firebase_options.dart';
 import 'package:flutter_recipeapp/screens/tabscreen.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
